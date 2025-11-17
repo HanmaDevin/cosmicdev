1f2796562d9ffee7e8a9fb76e36e99b98fdadb85
HanmaDevin <Hanma@Devin.com>
2025-11-17 08:57:11 +0100

    clean up after script

--- scripts/upfetch.sh
+++ scripts/upfetch.sh
@@ -2,3 +2,4 @@
 fetch_version=$(curl -s "https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest" | jq .tag_name | grep -Po --color=never "\d.\d+.\d")
 wget -O fetch.deb "https://github.com/fastfetch-cli/fastfetch/releases/download/${fetch_version}/fastfetch-linux-amd64.deb"
 sudo apt install ./fetch.deb
+rm ./fetch.deb
--- scripts/upnvim.sh
+++ scripts/upnvim.sh
@@ -2,4 +2,4 @@
 nvim_version=$(curl -s "https://api.github.com/repos/neovim/neovim/releases/latest" | jq .tag_name | grep -Po --color=never "\d.\d+.\d")
 wget -O nvim "https://github.com/neovim/neovim/releases/download/v${nvim_version}/nvim-linux-x86_64.appimage"
 chmod +x ./nvim
-sudo cp ./nvim /usr/bin/
+sudo mv ./nvim /usr/bin/
